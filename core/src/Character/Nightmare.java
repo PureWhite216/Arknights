05ed8c85a26b1c1d1bb2f9732b2ce38a9daa307c
zjy <1565010390@qq.com>
2022-06-14 15:39:46 +0800

    Nightmare

---
+++ core/src/Character/Nightmare.java
@@ -0,0 +1,56 @@
+package Character;
+
+import Audio.AudioManager;
+import Component.AnimationComponent;
+import Component.BattleComponent;
+import Component.Skill.Amiya.Skill_Amiya_PowerStrike;
+import Component.Skill.Liskarm.Skill_Liskarm_Attack;
+import Component.Skill.Nightmare.Skill_Nightmare_Attack;
+import Component.Skill.SkillBase;
+import Component.Skill.Amiya.Skill_Amiya_Attack;
+import Level.BattleLevelBase;
+import Level.LevelBase;
+import UI.SkillChooseTable;
+import com.badlogic.gdx.Gdx;
+
+public class Nightmare extends Operator{
+    /*Set up Assets Path*/
+    static final String atlasPath = "assets/Operators/Nightmare/nightmare.atlas";
+    static final String skelPath = "assets/Operators/Nightmare/nightmare.skel";
+    static final String soundPath = "assets/Operators/Nightmare/夜魔_选中干员1.wav";
+    static String[] skillSoundsPath;
+
+    public Nightmare(float posX, float posY)
+    {
+        super(posX, posY, defaultScale, soundPath, skillSoundsPath);
+        /*Set Battle Info*/
+        battleComponent = new BattleComponent(120, 120, 15, 30, this);
+        /*Init Skill*/
+        skills.add(new Skill_Nightmare_Attack(this));
+
+
+        /*Set up skillChooseTable*/
+        skillChooseTable = new SkillChooseTable(this);
+    }
+
+    @Override
+    public void enterLevel(BattleLevelBase currentLevel, int index)
+    {
+        super.enterLevel(currentLevel, index);
+        /*Init Animation*/
+        animationComponent.getAnimationState().addAnimation(0, "Idle", true, 0f);
+
+    }
+
+    @Override
+    protected void setAnimationComponent(float scale)
+    {
+        /*Set up Animation Component*/
+        animationComponent = new AnimationComponent(atlasPath, skelPath, scale);
+        /*Set Animation Mix*/
+        animationComponent.getAnimationStateData().setMix("Idle", "Attack", 0.5f);
+        animationComponent.getAnimationStateData().setMix("Start", "Idle", 0.3f);
+    }
+}
+
+
---
+++ core/src/Component/Skill/Nightmare/Skill_Nightmare_Attack.java
@@ -0,0 +1,51 @@
+package Component.Skill.Nightmare;
+
+import Audio.AudioManager;
+import Audio.SFXName;
+import Character.CharacterBase;
+import Character.Operator;
+import Component.DamageType;
+import Component.Skill.Skill_Attack;
+import Level.BattleLevelBase;
+import com.badlogic.gdx.math.Interpolation;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.actions.RunnableAction;
+
+public class Skill_Nightmare_Attack extends Skill_Attack
+{
+    public Skill_Nightmare_Attack(CharacterBase character)
+    {
+        super(character);
+        skillName = "法术攻击";
+    }
+
+    @Override
+    protected void callSound()
+    {
+        character.getSkillSounds()[0].play(0.5f);
+    }
+
+    @Override
+    protected void callEffect()
+    {
+        character.getTarget().getBattleComponent().getDamage(battleComponent.getAtk(), DamageType.Magical);
+        AudioManager.getInstance().getSFX().get(SFXName.magic).play(0.6f);
+    }
+
+    @Override
+    public void callSkill()
+    {
+//        callSound();
+
+        // Call Skeleton Animation
+        animationComponent.getAnimationState().setAnimation(0, "Attack", false);
+        animationComponent.getAnimationState().addAnimation(0, "Idle", true, 0f);
+        // Call Action
+        character.clearActions();
+        character.addAction(Actions.sequence(
+                Actions.delay(0.5f),
+                getEffectAction()
+        ));
+    }
+}
+
